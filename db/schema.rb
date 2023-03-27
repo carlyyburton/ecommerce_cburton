f4686743baa760467e01849ec4ccb3ee7bf1fb7f
carlyyburton <carlyyburton@gmail.com>
2023-03-27 14:22:21 -0500

    Created User model and table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_173315) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_192027) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -81,6 +81,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_173315) do
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
