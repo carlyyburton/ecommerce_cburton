a07000e9826c7c3348dea6d880e9d4bb38314a43
carlyyburton <carlyyburton@gmail.com>
2023-04-10 16:48:02 -0500

    Customer table added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  pay_customer stripe_attributes: :stripe_attributes
-  pay_customer default_payment_processor: :stripe
-
-  def stripe_attributes(pay_customer)
-    {
-      address: {
-        city: pay_customer.owner.city,
-        country: pay_customer.owner.country
-      },
-      metadata: {
-        pay_customer_id: pay_customer.id,
-        user_id: id
-      }
-    }
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :customers
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :categories
   resources :products
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
