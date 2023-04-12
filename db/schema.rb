8d96723340c9971b81903d817481696a35764994
carlyyburton <carlyyburton@gmail.com>
2023-04-11 20:50:39 -0500

    Added city to users model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_183118) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_014946) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -200,6 +200,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_183118) do
     t.string "first_name"
     t.string "last_name"
     t.integer "province_id"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
